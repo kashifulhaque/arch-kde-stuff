f0074ec61d64bc447fb4bc3900e7db818956a844
Kashiful Haque <haque.kashiful7@gmail.com>
2023-12-30 01:48:35 +0530

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+# Check if git is installed or not
+if ! command -v git &> /dev/null; then
+  echo "Git is not installed, installing git ..."
+  sudo pacman -Sy --noconfirm git
+
+  # Check if git install was a success or not
+  if ! command -v git &> /dev/null; then
+    echo "Failed to install git, check your internet maybe!"
+    exit 1
+  else
+    echo "Git installed successfully"
+  fi
+fi
+
+# Check if yay is installed or not
+if command -v yay &> /dev/null; then
+  echo "yay is already installed"
+else
+  # Clone yay repo
+  git clone https://aur.archlinux.org/yay.git
+  
+  # Build and install yay
+  cd yay
+  makepkg -si
+
+  # Check if yay was successfully installed
+  if command -v yay &> /dev/null; then
+    echo "yay has been installed successfully"
+  else
+    echo "Failed to install yay, check your internet maybe!"
+  fi
+
+  # Delete the yay repo from local machine
+  cd ..
+  rm -rf yay
+fi
+
+# Install some required softwares (I use them so yeah, feel free to modify the script)
+yay -S --noconfirm curl google-chrome spotify discord xwaylandvideobridge neofetch neovim-git nano visual-studio-code-bin htop bpytop freshfetch-git
+
+# Install node latest using volta
+curl https://get.volta.sh | bash
+source ~/.bashrc
+volta install node
+
+# Install miniconda (Assuming x86 system, again feel free to modify the script)
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+echo "Restart the terminal and paste 'conda config --set auto_activate_base false' to stop conda from activating base"
