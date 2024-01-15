581597773f9f9da204001397ce92af038f7caf49
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-15 19:13:53 +0530

    Added sdk man(Installer for Java)/Firefox

--- setup.sh
+++ setup.sh
@@ -45,12 +45,17 @@ yay -S --noconfirm curl
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Install some required softwares (I use them so yeah, feel free to modify the script)
-yay -S --noconfirm base-devel google-chrome spotify discord xwaylandvideobridge neofetch neovim-git nano visual-studio-code-bin htop bpytop freshfetch-git ttf-symbola noto-fonts-cjk noto-fonts-emoji ttf-twemoji fzf kcalc vlc okular tt
+yay -S --noconfirm base-devel google-chrome firefox spotify discord xwaylandvideobridge neofetch neovim-git nano visual-studio-code-bin htop bpytop freshfetch-git ttf-symbola noto-fonts-cjk noto-fonts-emoji ttf-twemoji fzf kcalc vlc okular tt 
 
 # Install node latest using volta
 curl https://get.volta.sh | bash
 source ~/.bashrc
 volta install node
+
+# Install java latest using SDKMAN
+curl -s "https://get.sdkman.io" | bash
+source ~/.bashrc
+sdk install java
 
 # Install miniconda (Assuming x86 system, again feel free to modify the script)
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
