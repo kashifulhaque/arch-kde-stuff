36d371a42e8742094e059f858c9c7e3a0822dba6
Yash Mehrotra <128923633+ReDxDaGer@users.noreply.github.com>
2024-01-16 15:10:53 +0530

    Added SDKMAN (JAVA install) / Firefox (#1)
    * Added sdk man(Installer for Java)/Firefox
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ else
   rm -rf yay
 fi
 
+echo "Installing CURL"
 # Install curl
 yay -S --noconfirm curl
 
@@ -45,13 +46,25 @@ yay -S --noconfirm curl
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Install some required softwares (I use them so yeah, feel free to modify the script)
-yay -S --noconfirm base-devel google-chrome spotify discord xwaylandvideobridge neofetch neovim-git nano visual-studio-code-bin htop bpytop freshfetch-git ttf-symbola noto-fonts-cjk noto-fonts-emoji ttf-twemoji fzf kcalc vlc okular tt
+yay -S --noconfirm base-devel google-chrome spotify discord xwaylandvideobridge neofetch neovim-git nano visual-studio-code-bin htop bpytop freshfetch-git ttf-symbola noto-fonts-cjk noto-fonts-emoji ttf-twemoji fzf kcalc vlc okular tt 
 
+echo "All the packages basically required are installed !!"
+
+echo "Now installing Volta and node latest"
 # Install node latest using volta
 curl https://get.volta.sh | bash
 source ~/.bashrc
 volta install node
 
+echo "Now installing SDKMAN and Java latest"
+# Install java latest using SDKMAN
+curl -s "https://get.sdkman.io" | bash
+source ~/.bashrc
+sdk install java
+
+echo "Java latets installed"
+
+echo "Now Installing Miniconda"
 # Install miniconda (Assuming x86 system, again feel free to modify the script)
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
