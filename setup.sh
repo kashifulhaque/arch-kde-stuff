4ab8765caf41b719de16ffea078d3d07975bf9bd
Ayush <123345456+Spirizeon@users.noreply.github.com>
2024-01-17 00:07:03 +0530

    Added more stuff, and removed some extra lines. (#2)
    ### Added
    ```
    yay -S podman-docker
    ```
    > If you want to replace [Docker](https://wiki-archlinux-org.translate.goog/title/Docker?_x_tr_sl=en&_x_tr_tl=hi&_x_tr_hl=hi&_x_tr_pto=tc), one can install [podman-docker](https://translate.google.com/website?sl=en&tl=hi&hl=hi&client=srp&u=https://archlinux.org/packages/?name%3Dpodman-docker) to mimic the docker binary along with man pages. - [Arch wiki](https://wiki-archlinux-org.translate.goog/title/Podman?_x_tr_sl=en&_x_tr_tl=hi&_x_tr_hl=hi&_x_tr_pto=tc)
    
    ### Removed
    commented out `source ~/.bashrc` on line 57 since line 61 already does the job
    
    ### Replaced: 
    ```
    #Install miniconda (Assuming x86 system, again feel free to modify the script)
    curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
    bash Miniconda3-latest-Linux-x86_64.sh
    ``` 
    with 
    ```
    yay -S miniconda3
    ```

--- setup.sh
+++ setup.sh
@@ -38,47 +38,25 @@ else
   rm -rf yay
 fi
 
-echo "Installing CURL"
-# Install curl
-yay -S --noconfirm curl
-
-# Install rust
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
-
 # Install some required softwares (I use them so yeah, feel free to modify the script)
-yay -S --noconfirm base-devel google-chrome spotify discord xwaylandvideobridge neofetch neovim-git nano visual-studio-code-bin htop bpytop freshfetch-git ttf-symbola noto-fonts-cjk noto-fonts-emoji ttf-twemoji fzf kcalc vlc okular tt 
+yay -S --noconfirm \
+  curl google-chrome \
+  spotify discord \
+  xwaylandvideobridge \
+  neofetch neovim-git \
+  visual-studio-code-bin \
+  htop bpytop freshfetch \
+  gcc clang go base-devel \
+  ttf-symbola noto-fonts-cjk \
+  noto-fonts-emoji ttf-twemoji \
+  podman-docker #install podman \
+  miniconda3 #install miniconda3
 
-echo "All the packages basically required are installed !!"
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#source ~/.bashrc # this is extra
 
-echo "Now installing Volta and node latest"
 # Install node latest using volta
 curl https://get.volta.sh | bash
 source ~/.bashrc
 volta install node
-
-echo "Now installing SDKMAN and Java latest"
-# Install java latest using SDKMAN
-curl -s "https://get.sdkman.io" | bash
-source ~/.bashrc
-sdk install java
-
-echo "Java latets installed"
-
-echo "Now Installing Miniconda"
-# Install miniconda (Assuming x86 system, again feel free to modify the script)
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-echo "Restart the terminal and paste 'conda config --set auto_activate_base false' to stop conda from activating base"
-
-# Install NvChad
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
-
-# Install docker (this script REBOOTS the system)
-yay -Sy
-yay -S docker docker-compose --noconfirm
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo reboot now
